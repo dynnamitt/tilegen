4f454f8f7993a09a1165558735086f88b6b43f0a
KFM <kjetil@docstream.no>
2016-12-28 21:34:58 +0100

    drawingWeAre

--- Tile.elm
+++ Tile.elm
@@ -4,5 +4,8 @@ import Svg exposing (..)
 import Svg.Attributes exposing (..)
 
 main = 
-  svg [ viewBox "-5 -5 10 10" ]
-      [ circle [ r "1" ] [] ]
+  svg [ viewBox "-50 -50 100 100" ]
+      [ 
+        circle [ r "50" , fill "none", stroke "blue" ] []
+      , circle [ r "1"] []
+      ]
